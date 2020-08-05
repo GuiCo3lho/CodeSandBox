5de06889427cf47fba1daae1efd0721f801061ef
guico3lho <37370101+GuiCo3lho@users.noreply.github.com>
2020-08-05 20:18:00 -0300

    initial commit

--- src/EffectExample.js
+++ src/EffectExample.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function EffectExample() {
   const [counter, setCounter] = useState(0);
+  //var counter2 = 3;
 
   useEffect(() => {
     var elemento = document.getElementById("nelson");
@@ -15,8 +16,27 @@ export default function EffectExample() {
       <h1 id="nelson">asdaasdsd</h1>
 
       <h1>{counter}</h1>
+
       <button onClick={() => setCounter(counter + 1)}>Clicar</button>
       <button onClick={() => setCounter(0)}>Resetar</button>
+
+      <button
+        onClick={e => {
+          //Why <button onClick={e=>{}}
+          //counter2 += 2;
+          //console.log(counter2);
+          setCounter(n => n + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={e => {
+          setCounter(n => n - 1);
+        }}
+      >
+        -
+      </button>
     </div>
   );
 }
